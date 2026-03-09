e74e39f271521a01aeaaac466454c64752b25669
Parilaksh <Parilakshthakur@gmail.com>
2026-03-09 22:30:26 +0530

    dev done

---
+++ task-1.js
@@ -0,0 +1,16 @@
+const arr = [23, 45, 67, 89, 12, 90, 44];
+
+let largest = -Infinity;
+let secondLargest = -Infinity;
+
+for (let num of arr) {
+  if (num > largest) {
+    secondLargest = largest;
+    largest = num;
+  } 
+  else if (num > secondLargest && num !== largest) {
+    secondLargest = num;
+  }
+}
+
+console.log("Second Largest Number:", secondLargest);
